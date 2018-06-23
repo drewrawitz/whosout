5522c3c5faace9bd6d40240e4613bf6f6938d434
Drew Rawitz <rawitz@sanbornagency.com>
2018-06-22 20:27:50 -0400

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  plugins: {
+    'postcss-easy-import': {},
+    'postcss-for': {},
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-custom-selectors': {},
+    'postcss-simple-vars': {},
+    'postcss-color-function': {},
+    'postcss-hexrgba': {},
+    'postcss-apply': {},
+    'postcss-calc': {},
+    'postcss-nested': {},
+    'postcss-atroot': {},
+    'postcss-assets': {
+      basePath: 'src/',
+      cachebuster: true,
+      loadPaths: ['img/'],
+      relative: true,
+    },
+    'postcss-inline-svg': {
+      path: 'src',
+    },
+    'postcss-initial': {},
+    'postcss-pseudo-class-enter': {},
+    'postcss-pxtorem': {
+      unitPrecision: 4,
+      propWhiteList: [],
+      replace: true,
+      selectorBlackList: [],
+      minPixelValue: 4,
+    },
+    autoprefixer: {
+      /* http://browserl.ist/?q=last+2+Chrome+versions%2C+last+2+Firefox+versions%2C+last+2+Safari+versions%2C+last+2+Edge+versions%2C+last+4+iOS+versions%2C+last+2+ChromeAndroid+versions */
+      browsers: [
+        'last 2 Chrome versions',
+        'last 2 Firefox versions',
+        'last 2 Safari versions',
+        'last 2 Edge versions',
+        'last 4 iOS versions',
+        'last 2 ChromeAndroid versions',
+      ],
+    },
+    cssnano: {
+      preset: [
+        'default',
+        {
+          calc: false,
+          colormin: false,
+          discardUnused: false,
+          normalizeUrl: false,
+        },
+      ],
+    },
+    'postcss-reporter': {
+      clearMessages: true,
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin('dist', {}),
+    new MiniCssExtractPlugin({ filename: 'styles.[contenthash].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
